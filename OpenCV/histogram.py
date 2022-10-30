5a560e5b9c77cfdfdf0d802badb17d3bcde7b844
YALIM GÜRBÜZ <99664429+ylmgrbzz@users.noreply.github.com>
2022-10-30 23:02:08 +0300

    Add files via upload

---
+++ OpenCV/histogram.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def custom_hist(gray):
+    h,w=gray.shape
+    hist=np.zeros([256],dtype=np.int32)
+    for row in range(h):
+        for col in range(w):
+            pv=gray[row,col]
+            hist[pv]+=1
+
+    y_pos=np.arange(0,256,1,dtype=np.int32)
+    plt.bar(y_pos,hist,align='center',color='r',alpha=0.5)
+    plt.xticks(y_pos,y_pos)
+    plt.ylabel("frekans")
+    plt.title("histogram")
+    plt.show()
+
+def image_hist(image):
+    cv.imshow("input",image)
+    color=("blue","green","red")
+    for i,color in enumerate(color):
+        hist=cv.calcHist([image],[i],None,[256],[0,256])
+        plt.plot(hist,color=color)
+        plt.xlim([0,256])
+    plt.show()
+
+src = cv.imread('averaging.jpg')
+
+cv.namedWindow("input",cv.WINDOW_AUTOSIZE)
+cv.imshow("input",src)
+cv.waitKey(1)
+
+gray=cv.cvtColor(src,cv.COLOR_BGR2GRAY)
+custom_hist(gray)
+image_hist(src)
